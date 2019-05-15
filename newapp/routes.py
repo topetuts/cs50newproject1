9c7204f78bde3baf589485a1aa4327d462e0c190
Chris <chok@test.com>
2019-05-15 08:26:37 +0000

    with templates

--- newapp/routes.py
+++ newapp/routes.py
@@ -1,6 +1,22 @@
 from newapp import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!--initnewapp folder"
+    return "Hello, World test test!"
+
+@app.route('/index2')
+def index2():
+    user = {'username': 'Chok'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', insidehtmluser=user, insidehtmlpost=posts)
